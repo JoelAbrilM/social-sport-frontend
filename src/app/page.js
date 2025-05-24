c5faea35bb026de8b67032add7d2961bbd5a132d
JoelAbrilM <joelabril20@outlook.com>
2025-05-24 12:13:19 -0600

    First commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>Página principal</h2>
+      <p>Esta es la ruta &quot;/&quot;.</p>
+      <button>
+        Ir a la página de About
+      </button>
+    </div>
+  );
+}
